689c8a7b5a411bb99c1016c289637fc4ad43a580
charros96 <88594194+charros96@users.noreply.github.com>
2022-01-30 19:42:43 -0800

    try 2

--- elgamal.py
+++ elgamal.py
@@ -12,10 +12,16 @@ def keygen():
 def encrypt(pk,m):
     r = random.randrange(1,q)
     c1 = pow(g,r,p)
-    c2 = pow(pow(pk,r)*m,1,p)
+    c2 = pow(pk,r,p)
+    c2 = pow(c2*m,1,p)
     return [c1,c2]
 
 def decrypt(sk,c):
-    m = pow(c[0]/pow(c[1],sk),1,p)
+    num = pow(c[1],1,p)
+    den = pow(c[0],-sk,p)
+    m=num*den
     return m
 
+pk,sk = keygen()
+c = encrypt(pk,1010)
+print(decrypt(sk,c))
